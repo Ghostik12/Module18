828d2dd5a0606d849580dd36e49b8e290cbf6b41
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2024-01-21 21:12:25 +0300

    18.1.1

---
+++ Module18/Module18/CarConveyor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module18
+{
+    class CarConveyor : Conveyor
+    {
+        public CarConveyor() 
+        {
+            _product = new Product("Car");
+        }
+        public override void BuildDoors()
+        {
+            _product["doors"] = "4";
+        }
+
+        public override void BuildEngine()
+        {
+            _product["engine"] = "150 л.с.";
+        }
+
+        public override void BuildFrame()
+        {
+            _product["frame"] = "Рама автомобиля";
+        }
+
+        public override void BuildWheels()
+        {
+            _product["wheels"] = "4";
+        }
+    }
+}
---
+++ Module18/Module18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Module18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarPlant carPlant = new CarPlant();
+
+            Conveyor carConveyor = new CarConveyor();
+            carPlant.Construct(carConveyor);
+            carConveyor.Product.Show();
+        }
+    }
+}
